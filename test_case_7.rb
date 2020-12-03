dc6ee486ffb17c4acaf53d42c54add8f30e7406e
uyenhoang42 <74896050+uyenhoang42@users.noreply.github.com>
2020-12-03 23:28:43 +0700

    Update test_case_7.rb

--- test_case_7.rb
+++ test_case_7.rb
@@ -17,7 +17,20 @@ begin
   # 3. Job Title: QA
   jobtitle = driver.find_element(id: "job-title")
   jobtitle.send_keys 'QA'
-  sleep 2
+  # 4. Highest level of education: College
+  checkbox = driver.find_element(id: "div.container form:nth-child(4) div.form-group div.input-group:nth-child(7) > div.col-sm-8.col-sm-offset-2:nth-child(3)")
+  checkbox.click
+  # 5. Sex: Male
+  checkbox = driver.find_element(id: "div.container form:nth-child(4) div.form-group div.input-group:nth-child(9) > div.col-sm-8.col-sm-offset-2:nth-child(2)")
+  checkbox.click
+  # 6. Year of experience: 2-4
+  options = driver.find_element(:id, "select-menu")
+  select_object = Selenium::WebDriver::Support::Select.new(options)
+  select_object.select_by(:value, "2")
+  # 7. Date: 27/10/2025
+  search_box = driver.find_element(:id, 'datepicker') 
+  search_box.send_keys ("27/10/2025")
+  sleep 5
 ensure
   driver.quit
 end 
